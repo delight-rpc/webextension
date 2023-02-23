e129173e72848beab65c86543fae97f9977ce914
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-23 13:52:29 +0000

    refactor: improve channel support

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import browser from 'webextension-polyfill'
 import * as DelightRPC from 'delight-rpc'
-import { isntNull } from '@blackglory/prelude'
+import { go, isntNull } from '@blackglory/prelude'
 
 export function createServer<IAPI extends object>(
   api: DelightRPC.ImplementationOf<IAPI>
@@ -16,27 +16,31 @@ export function createServer<IAPI extends object>(
   port.onMessage.addListener(handler)
   return () => port.onMessage.removeListener(handler)
 
-  async function handler(
+  function handler(
     message: unknown
   , sender: browser.Runtime.MessageSender
-  ): Promise<unknown> {
+  ): void | Promise<unknown> {
     if (sender.id === browser.runtime.id) {
       const req = message
 
       if (DelightRPC.isRequest(req) || DelightRPC.isBatchRequest(req)) {
-        const result = await DelightRPC.createResponse(
-          api
-        , req
-        , {
-            parameterValidators
-          , version
-          , channel
-          , ownPropsOnly
-          }
-        )
+        if (DelightRPC.matchChannel(req, channel)) {
+          return go(async () => {
+            const res = await DelightRPC.createResponse(
+              api
+            , req
+            , {
+                parameterValidators
+              , version
+              , channel
+              , ownPropsOnly
+              }
+            )
 
-        if (isntNull(result)) {
-          return result
+            if (isntNull(res)) {
+              return res
+            }
+          })
         }
       }
     }
